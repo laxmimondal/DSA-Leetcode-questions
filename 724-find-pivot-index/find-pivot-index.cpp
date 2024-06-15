259794840901acf55c3907a6193a36dbe3bb5d92
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-06-15 19:52:27 +0530

    Time: 10 ms (94.10%) | Memory: 34 MB (63.17%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum = sum +nums[i];
+        }
+        int cs = 0;
+        for (int x = 0; x < n ; x++){
+             int ls = cs;
+             int rs = sum - cs- nums[x];
+             // Check if the current index is the pivot index
+            if (ls == rs) {
+                return x;
+            } 
+            // Update the current sum
+            cs += nums[x];
+        }
+   // Return -1 if no pivot index is found
+        return -1;
+    }
+};
