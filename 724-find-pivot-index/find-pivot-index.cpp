873b3a029a26dd9b158f1fb4114b0c5720758b5c
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-06-15 20:00:28 +0530

    Time: 15 ms (72.46%) | Memory: 34.1 MB (34.37%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -8,15 +8,14 @@ public:
         }
         int cs = 0;
         for (int x = 0; x < n ; x++){
+           
              int ls = cs;
              int rs = sum - cs- nums[x];
-             cs += nums[x];
+              cs += nums[x];
              // Check if the current index is the pivot index
             if (ls == rs) {
                 return x;
-            } 
-            // Update the current sum
-            
+            }       
         }
    // Return -1 if no pivot index is found
         return -1;
