60fbd257198c27c602d39bd2b4c52267175b20e6
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-12-09 20:49:46 +0530

    Time: 2 ms (53.71%) | Memory: 8.5 MB (45.52%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers are not palindromes
-        if (x < 0) return false;
-
-        long long revNum = 0;  // Use long long to handle overflow
-        int dup = x;
-
-        while (x > 0) {
+      long long revNum = 0;  // Use long long to handle overflow
+       int dup = x;
+          while (x > 0) {
             int ld = x % 10;
             revNum = (revNum * 10) + ld;
 
             // Remove the last digit from x
-            x /= 10;
-
-            // Check if the reversed number exceeds INT_MAX
-            if (revNum > INT_MAX) return false;
+            x = x/ 10;
+           // Check if the reversed number exceeds INT_MAX
+            if (revNum > INT_MAX || revNum < INT_MIN) return false;
         }
 
         // Check if the original number is equal to its reverse
