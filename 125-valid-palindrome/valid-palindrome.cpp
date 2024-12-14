b6283e974f6ad001f0aee92e7a8a8642c5d59a6e
laxmimondal <158059694+laxmimondal@users.noreply.github.com>
2024-12-14 22:14:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (22.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,51 +1,27 @@
-#include <string>
+
 
 class Solution {
-private:
-    bool valid(char ch) {
-        if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9') {
-            return true;
-        }
-        return false;
-    }
-
-    char lower(char ch) {
-        if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9') {
-            return ch;
-        } else {
-            char temp = ch - 'A' + 'a';
-            return temp;
-        }
-    }
-
-    bool checkPalindrome(const std::string& a) {
-        int l = 0;
-        int r = a.size() - 1;
-        while (l <= r) {
-            if (a[l] != a[r]) {
-                return false;
-            } else {
-                l++;
-                r--;
-            }
-        }
-        return true;
-    }
-
 public:
     bool isPalindrome(std::string s) {
-        // Remove non-alphanumeric characters
-        std::string temp = "";
-        for (int j = 0; j < s.size(); j++) {
-            if (valid(s[j])) {
-                temp.push_back(s[j]);
-            }
+        int n = s.length();
+      int st = 0;
+      int e = n -1 ;
+      while( st < e){
+        if(!isalnum(s[st])){
+            st++;
         }
-        // Convert to lowercase
-        for (int j = 0; j < temp.size(); j++) {
-            temp[j] = lower(temp[j]);
+        else if(!isalnum(s[e])){
+            e--;
         }
-        // Check if the string is a palindrome
-        return checkPalindrome(temp);
+        else if(tolower(s[st]) != tolower(s[e])){
+           return false;
+        }
+        else{
+            st++;
+            e--;
+        }
+
+      }
+      return true ;
     }
 };
